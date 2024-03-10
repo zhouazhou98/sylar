b8f9f1f44939f4ac672294c24afaaf90323b6eec
zhou <916823108@qq.com>
2024-03-10 19:49:32 +0800

    sylar 08 - [日志系统] [日志管理器]
    之前忘了上传 log_manager.cc 文件

---
+++ zhou/log/log_manager.h
@@ -0,0 +1,43 @@
+#ifndef __ZHOU_LOG_MANAGER__H_
+#define __ZHOU_LOG_MANAGER__H_
+
+#include "log.h"
+#include <map>
+#include "zhou/utils/singleton.h"
+
+namespace zhou{     // LOG_MANAGER
+
+
+
+class LoggerManager {  
+public:
+    LoggerManager() {
+        m_root.reset(new Logger);
+        m_root->addAppender(LogAppender::ptr(new StdoutLogAppender));
+    }
+    Logger::ptr getLogger(std::string name) {
+        auto iter = m_loggers.find(name);
+        if (iter != m_loggers.end()) 
+            return iter->second;
+        
+        Logger::ptr logger(new Logger(name));
+        logger->setRoot(m_root);
+        m_loggers[name] = logger;
+        return logger;
+    }
+
+    void init();
+
+private:
+    std::map<std::string, Logger::ptr> m_loggers;
+    Logger::ptr m_root;
+
+};
+
+
+typedef Singleton<LoggerManager> SingleLoggerManager;
+
+}   // !LOG_MANAGER
+
+#endif  // !__ZHOU_LOG_MANAGER__H_
+
