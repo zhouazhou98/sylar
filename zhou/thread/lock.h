37e87cd24f90155e2b803791978645d5e7d4b6c6
zhou <916823108@qq.com>
2024-03-10 19:29:38 +0800

    sylar 22 - [线程模块] [互斥锁]
    封装了 pthread 互斥量 pthread_mutex_t： Mutex 类
    增加了 Mutex 的测试： test_lock.cc

--- zhou/thread/lock.h
+++ zhou/thread/lock.h
@@ -39,9 +39,22 @@ private:
 namespace zhou {    // Mutex
 class Mutex {
 public:
+    typedef ScopedLockImpl<Mutex> Lock;
+    Mutex() {
+        pthread_mutex_init(&m_mutex, nullptr);
+    }
+    ~Mutex() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+    void lock() {
+        pthread_mutex_lock(&m_mutex);
+    }
+    void unlock() {
+        pthread_mutex_unlock(&m_mutex);
+    }
 
 private:
-    pthread_mutex_t m_lock;
+    pthread_mutex_t m_mutex;
 };
 
 }   // ! Mutex
