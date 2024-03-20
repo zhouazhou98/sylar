c6e03050edd56b7b5d1ed28265dd17d5504649cd
zhouazhou98 <zhouazhou98@gmail.com>
2024-03-20 21:22:09 +0800

    sylar 33 - [协程调度模块] [BUG]
    BUG:
    目前当 use_caller == true  即生成调度器的线程也需要被调度器调度时，会出现 BUG
    
    问题： 排查后发现问题大概率出现在 use_caller 线程仍使用 MainFunc ，在最后执行完成后上下文没能正确切换， sylar 源码中使用了 CallerMainFunc 来解决这一现象

--- tests/test_scheduler.cc
+++ tests/test_scheduler.cc
@@ -4,18 +4,23 @@
 
 zhou::Logger::ptr g_logger = zhou::SingleLoggerManager::GetInstance()->getLogger("root");
 
+static zhou::Mutex mutex;
+
 void test() {
+    zhou::Mutex::Lock lock(mutex);
     static int test = 0;
     test++;
-    std::cout << "test: " << test << std::endl;
+    std::cout << "thread_id: " << syscall(SYS_gettid) << "\ttest: " << test << std::endl;
 
 }
 
 
 int main() {
-    zhou::Scheduler::ptr sc(new zhou::Scheduler(1, true, "hello"));
+    zhou::Scheduler::ptr sc(new zhou::Scheduler(3, false, "hello"));
     sc->start();
-    sc->schedule([](){ std::cout << "hello world!" << std::endl; });
+    sc->schedule([](){ zhou::Mutex::Lock lock(mutex); std::cout << "hello world!" << std::endl; });
+    sc->schedule(&test);
+    sc->schedule(&test);
     sc->schedule(&test);
     sc->stop();
     return 0;
