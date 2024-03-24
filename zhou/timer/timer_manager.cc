d32e9a41096fb62525c714c1ba693d9e58b50549
zhouazhou98 <zhouazhou98@gmail.com>
2024-03-24 22:44:14 +0800

    sylar 40 - [定时器] TimerManager 头文件

--- zhou/timer/timer_manager.cc
+++ zhou/timer/timer_manager.cc
@@ -3,10 +3,58 @@
 
 namespace zhou {
 
+TimerManager::TimerManager(){
+}
+TimerManager::~TimerManager(){
+}
+
+// 添加定时器
+//  ms: 定时器执行间隔时间
+//  callback: 回调函数
+//  recurring: 是否循环
+Timer::ptr TimerManager::addTimer(uint64_t ms, std::function<void()> callback, bool recurring = false){
+}
+
+// 添加条件定时器
+//  weak_condition: 条件
+Timer::ptr TimerManager::addConditionTimer(uint64_t ms, std::function<void()> callback, std::weak_ptr<void> weak_condition, bool recurring = false){
+}
+
+// 到最近一个将要执行的定时器执行的时间间隔
+uint64_t TimerManager::getNextTimer(){
+}
+
+// 获取需要执行的定时器的回调函数列表
+void TimerManager::listExpiredCallback(std::vector<std::function<void()>> & callbacks){
+}
+
+// 检查是否有定时器
+bool TimerManager::hasTime(){
+}
+
+
+// 添加定时器
+//  val: 定时器
+//  lock: 锁
+void TimerManager::addTimer(Timer::ptr val, RWMutexType::WriteLock& lock){
+}
+
+// 当有新的定时器插入到定时器的首部，执行该函数
+void TimerManager::onTimeInsertedAtFront(){
+}
+
+// 检测服务器时间是否被调后了
+bool TimerManager::detectClockRollover(uint64_t now_ms){
+}
+
+
 
 void TimerManager::addTimer(Timer::ptr val, RWMutexType::WriteLock& lock) {
 
 }
+
+
+
 
 }
 
--- zhou/timer/timer_manager.h
+++ zhou/timer/timer_manager.h
@@ -14,11 +14,47 @@ friend class Timer;
 
 public:
     typedef RWMutex RWMutexType;
+    
+    TimerManager();
+    virtual ~TimerManager();
+
+    // 添加定时器
+    //  ms: 定时器执行间隔时间
+    //  callback: 回调函数
+    //  recurring: 是否循环
+    Timer::ptr addTimer(uint64_t ms, std::function<void()> callback, bool recurring = false);
+
+    // 添加条件定时器
+    //  weak_condition: 条件
+    Timer::ptr addConditionTimer(uint64_t ms, std::function<void()> callback, std::weak_ptr<void> weak_condition, bool recurring = false);
+
+    // 到最近一个将要执行的定时器执行的时间间隔
+    uint64_t getNextTimer();
+
+    // 获取需要执行的定时器的回调函数列表
+    void listExpiredCallback(std::vector<std::function<void()>> & callbacks);
+
+    // 检查是否有定时器
+    bool hasTime();
+
+
+protected:
+    // 添加定时器
+    //  val: 定时器
+    //  lock: 锁
     void addTimer(Timer::ptr val, RWMutexType::WriteLock& lock);
 
+    // 当有新的定时器插入到定时器的首部，执行该函数
+    virtual void onTimeInsertedAtFront();
+
 private:
-    std::set<Timer::ptr, Timer::Comparator> m_timers;
-    RWMutexType m_mutex;
+    // 检测服务器时间是否被调后了
+    bool detectClockRollover(uint64_t now_ms);
+private:
+    RWMutexType m_mutex;                                // 锁
+    std::set<Timer::ptr, Timer::Comparator> m_timers;   // 定时器集合
+    bool m_tickled = false;                             // 是否触发 onTimerInsertedAtFront
+    uint64_t m_previousTime = 0;                        // 上次执行时间
 };
 
 }
